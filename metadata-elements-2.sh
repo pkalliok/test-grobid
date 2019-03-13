0dd933295c7749b33beb9c0452dc287f78718af6
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2019-03-13 20:55:04 +0200

    Metadata names for all fields

--- metadata-elements-2.sh
+++ metadata-elements-2.sh
@@ -7,5 +7,6 @@ sort | uniq -c  |
 sed 's#{http[^}]*}##g' |
 egrep -vw 'appInfo|application|availability|biblStruct|encodingDesc|fileDesc|imprint|monogr|profileDesc|publicationStmt|ref|sourceDesc|TEI|teiHeader|title/level|titleStmt|key|unit|type|address|addrLine|country|p|postBox|abstract/lang|analytic|persName' |
 sort -rn |
-sed 's#text/lang#kieli#;s#title#nimeke#;s#author#tekijä#;s#surname#tekijän sukunimi#;s#forename#tekijän etunimi#;s#note#tunnistamaton tieto#;s#date/when#julkaisupäivä (määr.)#;s#date#julkaisupäivä#;s#orgName#tekijäorganisaatio#;s#affiliation#tekijän taustaorg.#;s#abstract#tiivistelmä#;s#roleName#roolitermi#;s#meeting#julkaisupaikka#;s#biblScope#numero sarjassa#'
+sed 's#text/lang#kieli#;s#title/lang#nimekkeen kieli#;s#title#nimeke#;s#author#tekijä#;s#surname#tekijän sukunimi#;s#forename#tekijän etunimi#;s#note#tunnistamaton tieto#;s#date/when#julkaisupäivä (määr.)#;s#date#julkaisupäivä#;s#orgName#organisaation nimi#;s#affiliation#tekijän taustaorg.#;s#abstract#tiivistelmä#;s#roleName#roolitermi#;s#meeting#julkaisupaikka#;s#biblScope#numero sarjassa#' |
+awk '{ print $2 " " $3 "\t" $1; }'
 
